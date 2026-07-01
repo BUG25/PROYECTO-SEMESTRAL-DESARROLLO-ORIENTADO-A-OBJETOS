0abb5b398db564174aef5edeb0d82cc0b7e89c3e
Fefaarriagadav <jarriagada2025@udec.cl>
2026-07-01 10:22:04 -0400

    clase Perro extiende Mascota

---
+++ src/model/Perro.java
@@ -0,0 +1,18 @@
+package model;
+import habitat.TipoHabitat;
+
+/**
+ * Especie : Perro
+ * Habitat: Perrera
+ */
+
+public class Perro extends Mascota {
+    public Perro(String nombre){
+        super(nombre);
+    }
+    @Override
+    public TipoHabitat getTipoHabitatRequerido() {
+        return TipoHabitat.PERRERA;
+    }
+
+}
