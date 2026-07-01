627e80070fdb70d22b0410d8803740e4e4b242ee
Fefaarriagadav <jarriagada2025@udec.cl>
2026-07-01 16:56:56 -0400

    agrega clase tienda de donde se adquieren las cosas

---
+++ src/servicio/Tienda.java
@@ -0,0 +1,10 @@
+package servicio;
+import  factory.FabricaMascotas;
+
+/**
+ * representa la tienda
+ */
+
+public class Tienda {
+    private final FabricaMascotas fabricaMascotas = new FabricaMascotas();
+}
