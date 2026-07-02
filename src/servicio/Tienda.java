ca97949fe929eac60d8cb2d2a55564c701a9adbd
Fefaarriagadav <jarriagada2025@udec.cl>
2026-07-02 15:27:59 -0400

    agrega adoptarMascota a tienda con la verificación de si esta el habitat necesario para adoptar

--- src/servicio/Tienda.java
+++ src/servicio/Tienda.java
@@ -1,8 +1,11 @@
 package servicio;
+import excepciones.SinHabitatDisponibleException;
 import  factory.FabricaMascotas;
 import excepciones.DineroInsuficienteException;
 import habitat.Habitat;
 import habitat.TipoHabitat;
+import model.Mascota;
+import model.TipoMascota;
 
 /**
  * representa la tienda, valida las reglas ( que haya suficiente dinero para comprar y habitat necesaria)
@@ -29,5 +32,40 @@ public class Tienda {
         }
         usuario.descontarDinero(costo);
         usuario.getHabitats().add(new Habitat(tipo,capacidad));
+    }
+    /**
+     * EL usuario adopta a una mascota nueva. Se crea la mascotacon el factory method, y luego se analiza entre
+     * los habitats del usuario, se verifica que tenga el habitat correcto con capacidad suficiente
+     * si no existe se lanza SinHabitatDisponibleException y no se puede adoptar a la mascota
+     * @param usuario, quien adopta
+     * @param tipo, especie que se quiere adoptar
+     * @param nombre, nombre que se le da a la mascota
+     * @return, la mascota recien adoptada
+     * @throws excepciones.SinHabitatDisponibleException, si no hay habitat compatible con la especie
+     */
+    public Mascota adoptarMascota(Usuario usuario, TipoMascota tipo, String nombre)
+        throws SinHabitatDisponibleException{
+        Mascota nueva = fabricaMascotas.crear(tipo, nombre);
+        TipoHabitat tipoRequerido = nueva.getTipoHabitatRequerido();
+        Habitat habitatLibre = buscarHabitatConEspacio(usuario,tipoRequerido);
+        if(habitatLibre == null){
+            throw new SinHabitatDisponibleException(
+                    "No tienes un" + tipoRequerido + "con espacio para adoptar a " + tipo);
+        }
+        habitatLibre.ocupar();
+        usuario.getMascotas().add(nueva);
+        return nueva;
+    }
+    /**
+     * Bisca entre los habitats del usuario, que si tiene el habitat requerido y si tiene espacio libre
+     * @return el habitat encontrado, sino debuelve null
+     */
+    private Habitat buscarHabitatConEspacio(Usuario usuario, TipoHabitat tipoRequerido){
+        for (Habitat h : usuario.getHabitats()){
+            if (h.getTipo() == tipoRequerido && h.tieneEspaciosLibre()){
+                return h;
+            }
+        }
+        return null;
     }
 }
